c4c000e004d044cab758dd3814bdf3c752f632d7
theo.badoz <theo.badoz@gmail.com>
2024-06-04 10:44:42 +0200

    refacto: deprecation warning

--- utils/create_database.py
+++ utils/create_database.py
@@ -1,8 +1,8 @@
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 import os
 import shutil
