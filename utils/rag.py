802000ac964d90ccbf5a034510f983177d1f0845
theo.badoz <theo.badoz@gmail.com>
2024-06-04 11:41:22 +0200

    refacto: formatted response and sources

--- utils/rag.py
+++ utils/rag.py
@@ -1,8 +1,8 @@
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
@@ -12,7 +12,8 @@ MODEL = "gpt-3.5-turbo"
 MODEL = "mixtral:8x7b"
 PROMPT_TEMPLATE = """
 Réponds à la question posée ci-dessous en te basant sur le contexte fournit.
-Si tu ne peux pas répondre précisément à la question, réponds "je ne sais pas".
+Si tu ne peux pas répondre précisément à la question, précise "Le contexte fournit ne me permet pas de répondre précisément à votre question".
+Mais donne la réponse la plus probable selon toi.
 
 Contexte: {context}
 
@@ -20,27 +21,28 @@ Question: {question}
 """
 
 
-def answer_question(query_text):
+def get_context(query_text, db_path):
     # Prepare the DB
     embeddings = OpenAIEmbeddings()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embeddings)
+    db = Chroma(persist_directory=db_path, embedding_function=embeddings)
 
-    # Search the DB.
+    # Search the DB
     results = db.similarity_search(query_text, k=3)
-    # if len(results) == 0 or results[0][1] < 0.7:
-    #     st.markdown(f"Unable to find matching results.")
-    #     return
+    return results
 
+
+def answer_question(query_text):
+    results = get_context(query_text=query_text, db_path=CHROMA_PATH)
+    sources = [(doc.metadata.get("source", None), doc.metadata.get("page", None)) for doc in results]
+    fromatted_sources = "\n\n".join([source + ' à la page: ' + str(page+1) for source, page in sources])
+    
     context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     
-    # st.markdown(prompt)
-
     model = ChatOpenAI(model="gpt-3.5-turbo")
     response_text = model.predict(prompt)
 
-    # sources = [doc.metadata.get("source", None) for doc in results]
-    # formatted_response = f"Response: {response_text}\nSources: {sources}"
+    formatted_response = f"{response_text}\n\n\nLes documents les plus pertinents utilisés pour cette réponse:\n\n {fromatted_sources}"
     
-    return response_text
+    return formatted_response
