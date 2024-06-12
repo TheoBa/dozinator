ab32202441d9c14c999668f83dea40456b1f69b1
theo.badoz <theo.badoz@gmail.com>
2024-06-12 16:56:50 +0200

    bugfix: debug streamlit cloud with chroma db depandancy to sqlite

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from utils.rag import answer_question
 
+# debug streamlit cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 st.set_page_config(page_title='DOZinator', page_icon='📚', layout="wide")
 
 
